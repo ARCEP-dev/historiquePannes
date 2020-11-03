81a9aeed274426afe814bdcb6c1abcdb207e4d3a
GFerey <gaspard.ferey@arcep.fr>
2020-11-03 09:43:20 +0100

    Prototype of script added. First README.

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# historiquePannes
+# Enregistrement de l'historique des antennes en pannes tous opérateurs
+
+## Contexte
+
+Dans le cadre des accords du
+[New Deal Mobile](https://arcep.fr/cartes-et-donnees/tableau-de-bord-du-new-deal-mobile.html#NetworkStatus),
+les quatres opérateurs (Orange, Free, SFR et Bouygues Telecom) publient chaque jour un fichier
+qui répertorie leur antennes actuellement indisponibles.
+
+Ces données sont délivrées dans un format brut (pas de visualisation), sans historique (analyse temporelle impossible)
+et dans des formats différents (analyse comparative difficile).
+L'Arcep a donc développé un script de récupération quotidienne et d'harmonisation des données
+qu'elle met ici à disposition.
+
+Une [cartorgaphie minimaliste](https://arcep-dev.github.io/siteshs) est également mise à disposition
+et permet de visualiser cet historique des pannes à compter du ???
+
+## Contacts
+
+- Gaspard FEREY (Arcep) - gaspard [point] ferey [chez] arcep [point] fr
+- Romain MAZIERE (Arcep) - romain [point] maziere [chez] arcep [point] fr
+
+## Open Data
+
+Les données ne sont pas encore publiées en opendata.
+
+## Données sources
+
+Les fichiers publiées par les opérateurs:
+
+| Opérateur        | URL |
+|------------------|-----|
+| Free             | https://mobile.free.fr/moncompte/index.php?page=csv-antennes-relais-indisponibles |
+| Orange           | https://suivi-des-incidents.orange.fr/liste_des_antennes_en_panne.csv |
+| SFR              | https://static.s-sfr.fr/media/export-arcep/siteshorsservices.csv |
+| Bouygues Telecom | http://antennesindisponibles.bouyguestelecom.fr/antennesindisponibles.xls |
+
+## Langages
+
+- [Python3](https://www.python.org/)
+
+## Bibliothèqes
+
+- [GeoPandas](https://geopandas.org)
+
+
+
+## Utilisation
+
+
+## Installation du job cron
+
+- Simplement ajouter la ligne suivante dans votre fichier crontab:
+```bash
+13 6 * * * root python3 /siteshs.py /dumps/ >> /var/log/cron.log 2>&1
+```
---
+++ siteshs.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Romain MAZIERE and Gaspard FEREY (ARCEP)
+# Workflow to generate the shapefiles for the opendata
+# Requires Python 3.7
+
+from os import path, mkdir, sep
+import sys, time, math, re, json, requests
+from datetime import date, datetime
+import    pandas as  pd
+import geopandas as gpd
+from sqlalchemy import create_engine,MetaData,Table
+from sqlalchemy.sql import select,delete,insert,text
+
+#datename= str(date.today()) + "_" + str(datetime.now().hour) + "-" + str(datetime.now().minute)
+datename= str(date.today())
+
+print("")
+print("################################################")
+print("    Lancement du script à la date du ",datename)
+print("################################################")
+print("")
+
+# Chemin vers le dossier de sauvegarde
+savepath = sys.argv[1]
+# L'achitecture est la suivante (les dossiers sont créés s'ils sont inexistants):
+# savepath
+#   free
+#     01-01-2021
+#       01-01-2021_free.csv
+#       01-01-2021_free.json
+#       01-01-2021_free_raw.csv  (fichier téléchargé)
+#     ...                        (idem pour chaque date)
+#   ...                          (idem pour chaque operateur)
+#   all
+#     01-01-2021                 
+#       01-01-2021.csv           (fichier tout opérateurs concaténés)
+#       01-01-2021.json          (fichier tout opérateurs concaténés)
+#     ...                        
+
+# Liste des opérateurs avec les urls de téléchargement des fichiers, les formats ainsi que la correspondance des colonnes
+operateurs = [
+  {
+    "name":"free",
+    "url": "https://mobile.free.fr/moncompte/index.php?page=csv-antennes-relais-indisponibles",
+    "type": "csv",
+    "separator": ",",
+    "skipheader":0,
+    "skipfooter": 0,
+    "structure":{
+      "Dpt"       : "departement",
+      "cp"        : "code_postal",
+      "localite"  : "commune",
+      "latitude"  : "lat",
+      "longitude" : "long",
+      "Voix"      : "voix",
+      "Data"      : "data"
+    }
+  },
+  {
+    "name":"orange",
+    "url": "https://suivi-des-incidents.orange.fr/liste_des_antennes_en_panne.csv",
+    "type": "csv",
+    "separator": ";",
+    "skipheader":1,
+    "skipfooter": 0,
+    "structure":{
+      "Departement"       : "departement",
+      "Commune"           : "commune",
+      "Latitude"          : "lat",
+      "Longitude"         : "long",
+      "Service Voix/SMS"  : "voix",
+      "Service de Donnees": "data"
+    }
+  },
+  {
+    "name":"sfr",
+    "url": "https://static.s-sfr.fr/media/export-arcep/siteshorsservices.csv",
+    "type": "csv",
+    "separator": ";",
+    "skipheader":3,
+    "skipfooter": 8,
+    "structure":{
+      "Departement"       : "departement",
+      "code_insee_commune": "code_insee",
+      "Commune"           : "commune",
+      "X_WGS84_GPS"       : "lat",
+      "Y_WGS84_GPS"       : "long",
+      "Voix/SMS"          : "voix",
+      "internet mobile"   : "data"
+    }
+  },
+  {
+    "name":"bytel",
+    "url": "http://antennesindisponibles.bouyguestelecom.fr/antennesindisponibles.xls",
+    "type": "xls",
+    "excelsheet": 0,
+    "excelheader": 4,
+    "skipheader":0,
+    "skipfooter": 0,
+    "structure":{
+      "CODE POSTAL": "code_postal",
+      "COMMUNE"    : "commune",
+      "X (L93)"    : "x",
+      "Y (L93)"    : "y",
+      "Service Voix\n(2G et 3G Hors Service)" : "voix",
+      "Service d'accès à Internet et Voix sur LTE\n(3G et 4G Hors Service)" : "data"
+    }
+  }
+]
+
+def op_folder(op,date):
+    return savepath+sep+op['name']+sep+date
+def op_path(op,date,suffix):
+    return op_folder(op,date)+sep+date+'_'+op['name']+suffix
+def all_path(date,suffix):
+    return savepath+sep+'all'+sep+date+sep+date[0:10]+suffix
+def raw_path(op,date):
+    return op_path(op,date,'_raw.'+op['type'])
+
+def create_if_not_exists(folder):
+    if not path.isdir(folder):
+        mkdir(folder)
+
+create_if_not_exists(savepath+sep+'all')
+create_if_not_exists(savepath+sep+'all'+sep+datename)
+# Création d'un répertoire par opérateur
+for op in operateurs:
+    create_if_not_exists(savepath+sep+op['name'])
+    create_if_not_exists(op_folder(op,datename))
+
+
+def try_download(op):
+    r = requests.get(op["url"], allow_redirects=True)
+    if(r.status_code != 200):
+        return False
+    else:
+        print("Fichier téléchargé.")
+        # sauvegarde sur le disque
+        export_file = raw_path(op,datename)
+        open(export_file,"wb").write(r.content)
+        print( "Sauvegardé à " + export_file)
+        return True
+
+def download(op, maxtry):
+    for i in range(10):
+        print("Tentative :",i+1)
+        if try_download(op):
+            print("Succès du téléchargement !")
+            return True
+        else:
+            print("Echec de téléchargement !")
+            time.sleep(5)
+
+# Pour chaque opérateur de la liste :
+for op in operateurs:
+    print("Téléchargement de " + op['name'] + " : " + op["url"])
+    download(op)
+
+# Transformation des NaN en None
+def nonify(e):
+    return e if str(e) != 'nan' else None
+
+# Fonction de récupération d'un dataframe brut
+def get_raw_dataframe(op):
+    if op["type"] == "xls":
+        return pd.read_excel(raw_path(op,datename),
+                             sheet_name= op["excelsheet"],
+                             header    = op["excelheader"],
+                             index_col = None)
+    else:
+        return pd.read_csv(raw_path(op,datename),
+                           sep        = op["separator"],
+                           skiprows   = op["skipheader"],
+                           skipfooter = op["skipfooter"],
+                           engine = 'python')
+
+# Uniformisation des fichiers
+for op in operateurs:
+    print("Opérateur : " + op["name"])
+    df = get_raw_dataframe(op)
+    print("Sites HS : " + str(len(df.index)))
+    
+    #Renommage des colonnes
+    df.rename(columns=op["structure"], inplace=True)
+
+    header = ["date","operateur","departement","code_postal","code_insee","commune","lat","long","voix","data"]
+    nf = pd.DataFrame(columns=header)
+    
+    if ("lat" not in df or "long" not in df):
+        # Reprojection en WGS84 des coordonées projetées
+        pt = gpd.GeoDataFrame(geometry=gpd.points_from_xy(df["x"], df["y"]))
+        pt.crs = {"init" :"epsg:2154"}
+        pts = pt.to_crs({"init": "epsg:4326"})
+        nf["lat" ] = pts.geometry.y
+        nf["long"] = pts.geometry.x
+    else:
+        nf["lat"]  = df["lat"]
+        nf["long"] = df["long"]
+        
+    for col in ["code_insee", "code_postal"]:
+        # Formatage du code postal et/ou code insee
+        if col in df:
+            nf[col] = df[col].astype(str).str.zfill(5)
+            if "departement" not in df:
+                nf["departement"] = nf[col].str[0:2]
+        
+    # Extraction du code département via le code postal ou le code insee
+    if "departement" in df:
+        if df["departement"].dtypes == "int64":
+            df = df.astype({"departement": str})
+        nf["departement"] = [ re.findall('([0-9][0-9AB]?).*', d)[0] for d in df["departement"] ]
+    
+    for col in ["commune","voix","data"]:
+        nf[col] = df.get(col)
+    
+    # Tri des données
+    nf = nf.sort_values(by=["departement", "code_insee", "code_postal"])
+    nf["date"]     = datename[0:10]
+    nf["operateur"]= op['name']
+    
+    op['dataframe'] = nf
+    
+    # Ecriture du fichier au format standardisé csv (Un format unique pour les gouverner tous !)
+    nf.to_csv(op_path(op,datename,'.csv'), sep=',', index=False)
+    # Export en JSON (bon ok, deux...)
+    nf.to_json(op_path(op,datename,'.json'), orient="records")
+
+union_df = pd.concat( [ op['dataframe'] for op in operateurs ])
+union_df.to_csv(all_path(datename,'.csv'), sep=',', index=False)
+union_df.to_json(all_path(datename,'.json'), orient="records")
+
+# Conversion en GeoJSON
+geojson_properties = ["operateur", "departement","code_postal","code_insee","commune","voix","data"]
+def df_to_geojson(df, properties, lat='lat', lon='long'):
+    return {
+        'type':'FeatureCollection',
+        'features':
+            [
+                {  'type':      'Feature',
+                   'properties':{ prop : nonify(row[prop]) for prop in properties },
+                   'geometry':  {'type':'Point',
+                                 'coordinates':[row[lon],row[lat]]}}
+                for _, row in df.iterrows()
+            ]
+    }
+
+# Export en GeoJSON
+with open(all_path(datename,'.geojson'), "w") as file:
+    # Export dans le fichier au format geojson
+    geojson = df_to_geojson(union_df, geojson_properties)
+    file.write(json.dumps(geojson))
+
+print("Fichiers de données générés !")
