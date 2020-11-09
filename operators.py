af3576a8b7473031230c2623d295550acaf66f02
GFerey <gaspard.ferey@arcep.fr>
2020-11-09 13:56:22 +0100

    Update.

---
+++ operators.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Romain MAZIERE and Gaspard FEREY (ARCEP)
+# Workflow to generate the shapefiles for the opendata
+# Requires Python 3.7
+
+# Liste des opérateurs avec les urls de téléchargement des fichiers, les formats ainsi que la correspondance des colonnes
+operateurs = [
+  {
+    "name":"free",
+    "url": "https://mobile.free.fr/moncompte/index.php?page=csv-antennes-relais-indisponibles",
+    "type": "csv",
+    "separator": ",",
+    "skipheader":0,
+    "skipfooter": 0,
+    "structure":{
+      "Dpt"       : "departement",
+      "cp"        : "code_postal",
+      "localite"  : "commune",
+      "latitude"  : "lat",
+      "longitude" : "long",
+      "Voix"      : "voix",
+      "Data"      : "data"
+    }
+  },
+  {
+    "name":"orange",
+    "url": "https://suivi-des-incidents.orange.fr/liste_des_antennes_en_panne.csv",
+    "type": "csv",
+    "separator": ";",
+    "skipheader":1,
+    "skipfooter": 0,
+    "structure":{
+      "Departement"       : "departement",
+      "Commune"           : "commune",
+      "Latitude"          : "lat",
+      "Longitude"         : "long",
+      "Service Voix/SMS"  : "voix",
+      "Service de Donnees": "data"
+    }
+  },
+  {
+    "name":"sfr",
+    "url": "https://static.s-sfr.fr/media/export-arcep/siteshorsservices.csv",
+    "type": "csv",
+    "separator": ";",
+    "skipheader":3,
+    "skipfooter": 8,
+    "structure":{
+      "Departement"       : "departement",
+      "code_insee_commune": "code_insee",
+      "Commune"           : "commune",
+      "X_WGS84_GPS"       : "lat",
+      "Y_WGS84_GPS"       : "long",
+      "Voix/SMS"          : "voix",
+      "internet mobile"   : "data"
+    }
+  },
+  {
+    "name":"bytel",
+    "url": "http://antennesindisponibles.bouyguestelecom.fr/antennesindisponibles.xls",
+    "type": "xls",
+    "excelsheet": 0,
+    "excelheader": 4,
+    "skipheader":0,
+    "skipfooter": 0,
+    "structure":{
+      "CODE POSTAL": "code_postal",
+      "COMMUNE"    : "commune",
+      "X (L93)"    : "x",
+      "Y (L93)"    : "y",
+      "Service Voix\n(2G et 3G Hors Service)" : "voix",
+      "Service d'accès à Internet et Voix sur LTE\n(3G et 4G Hors Service)" : "data"
+    }
+  }
+]
--- siteshs.py
+++ siteshs.py
@@ -10,10 +10,9 @@ import sys, time, math, re, json, requests
 from datetime import date, datetime
 import    pandas as  pd
 import geopandas as gpd
-from sqlalchemy import create_engine,MetaData,Table
-from sqlalchemy.sql import select,delete,insert,text
 
-#datename= str(date.today()) + "_" + str(datetime.now().hour) + "-" + str(datetime.now().minute)
+from operators import operateurs
+
 datename= str(date.today())
 
 print("")
@@ -38,77 +37,6 @@ savepath = sys.argv[1]
 #       01-01-2021.csv           (fichier tout opérateurs concaténés)
 #       01-01-2021.json          (fichier tout opérateurs concaténés)
 #     ...                        
-
-# Liste des opérateurs avec les urls de téléchargement des fichiers, les formats ainsi que la correspondance des colonnes
-operateurs = [
-  {
-    "name":"free",
-    "url": "https://mobile.free.fr/moncompte/index.php?page=csv-antennes-relais-indisponibles",
-    "type": "csv",
-    "separator": ",",
-    "skipheader":0,
-    "skipfooter": 0,
-    "structure":{
-      "Dpt"       : "departement",
-      "cp"        : "code_postal",
-      "localite"  : "commune",
-      "latitude"  : "lat",
-      "longitude" : "long",
-      "Voix"      : "voix",
-      "Data"      : "data"
-    }
-  },
-  {
-    "name":"orange",
-    "url": "https://suivi-des-incidents.orange.fr/liste_des_antennes_en_panne.csv",
-    "type": "csv",
-    "separator": ";",
-    "skipheader":1,
-    "skipfooter": 0,
-    "structure":{
-      "Departement"       : "departement",
-      "Commune"           : "commune",
-      "Latitude"          : "lat",
-      "Longitude"         : "long",
-      "Service Voix/SMS"  : "voix",
-      "Service de Donnees": "data"
-    }
-  },
-  {
-    "name":"sfr",
-    "url": "https://static.s-sfr.fr/media/export-arcep/siteshorsservices.csv",
-    "type": "csv",
-    "separator": ";",
-    "skipheader":3,
-    "skipfooter": 8,
-    "structure":{
-      "Departement"       : "departement",
-      "code_insee_commune": "code_insee",
-      "Commune"           : "commune",
-      "X_WGS84_GPS"       : "lat",
-      "Y_WGS84_GPS"       : "long",
-      "Voix/SMS"          : "voix",
-      "internet mobile"   : "data"
-    }
-  },
-  {
-    "name":"bytel",
-    "url": "http://antennesindisponibles.bouyguestelecom.fr/antennesindisponibles.xls",
-    "type": "xls",
-    "excelsheet": 0,
-    "excelheader": 4,
-    "skipheader":0,
-    "skipfooter": 0,
-    "structure":{
-      "CODE POSTAL": "code_postal",
-      "COMMUNE"    : "commune",
-      "X (L93)"    : "x",
-      "Y (L93)"    : "y",
-      "Service Voix\n(2G et 3G Hors Service)" : "voix",
-      "Service d'accès à Internet et Voix sur LTE\n(3G et 4G Hors Service)" : "data"
-    }
-  }
-]
 
 def op_folder(op,date):
     return savepath+sep+op['name']+sep+date
@@ -144,7 +72,7 @@ def try_download(op):
         return True
 
 def download(op, maxtry):
-    for i in range(10):
+    for i in range(maxtry):
         print("Tentative :",i+1)
         if try_download(op):
             print("Succès du téléchargement !")
@@ -156,7 +84,7 @@ def download(op, maxtry):
 # Pour chaque opérateur de la liste :
 for op in operateurs:
     print("Téléchargement de " + op['name'] + " : " + op["url"])
-    download(op)
+    download(op, 10)
 
 # Transformation des NaN en None
 def nonify(e):
